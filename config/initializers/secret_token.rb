e7094a7c088253a2f2daddde8906bc5b174746cb
tksh <tksh@tksh-no-MacBook-Pro.local>
2013-10-27 13:38:45 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '28369e52ef708363541b3de76df9ec6706bb858cd9cb3c156a32ce1d5256f6b0041693d9f6e0d212750a99d05aaaee68677e84463ef7d2e5b9280bb8daf9c1e3'
